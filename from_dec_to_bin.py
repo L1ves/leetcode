9ddbb4bf5fe522807d291e4861a7be85274055ff
std-022-019 std-022-019 <std-022-019@praktikum-services.ru>
2023-12-08 12:39:50 +0500

    8.12.2023

---
+++ from_dec_to_bin.py
@@ -0,0 +1,3 @@
+def add_binary(a,b):
+    #your code here
+    return (bin(a+b)[2:])
---
+++ sum_array.py
@@ -0,0 +1,12 @@
+# Write a function that takes an array of numbers and returns the sum of the numbers.
+# The numbers can be  negative or non-integer. If the array does not
+# contain any numbers then you should return 0.
+def sum_array(a):
+    return sum(a) if len(a) > 0 else 0
+
+# def sum_array(a):
+#   return sum(a)
+#
+# def sum_array(a):
+#     return sum(a) if a else 0
+                                                                                                                         4,15          Top
