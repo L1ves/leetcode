59bfcca5dc9576249bbc9123988641a8982b64a9
L1ves <pro194@yandex.ru>
2024-05-10 11:53:18 +0500

    10.05.2024 go

---
+++ rockPaperScissor.go
@@ -0,0 +1,17 @@
+package kata
+
+
+func Rps(p1, p2 string) string {
+    if p1 == "paper" && p2 == "paper" {
+        return "Draw!"
+    } else if p1 == "scissors" && p2 == "scissors" {
+        return "Draw!"
+    } else if p1 == "rock" && p2 == "rock" {
+        return "Draw!"
+    } else if p1 == "scissors" && p2 == "paper" || p1 == "paper" && p2 == "rock" || p1 == "rock" && p2 == "scissors" {
+        return "Player 1 won!"
+    } else if p2 == "scissors" && p1 == "paper" || p2 == "paper" && p1 == "rock" || p2 == "rock" && p1 == "scissors" {
+      return "Player 2 won!"
+    }
+  return "Draw!"
+}
