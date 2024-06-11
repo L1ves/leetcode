64df098cb8ead3482d04e942289e6e9592c62fa2
L1ves <pro194@yandex.ru>
2024-06-11 11:54:22 +0500

    11.06.2024

---
+++ find_all_active_students.sql
@@ -0,0 +1,7 @@
+-- Type your query here
+--SELECT * FROM students
+--WHERE IsActive = 1;
+
+
+SELECT * FROM students
+WHERE IsActive;
---
+++ not_paied.sql
@@ -0,0 +1,5 @@
+select *
+from students
+where not tuition_received;
+
+--SELECT * from students where tuition_received = FALSE
