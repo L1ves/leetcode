683e5d56293844658c994d659f6376e242fe0bcb
Skvortsov <pro194@yandex.ru>
2024-07-30 10:22:02 +0500

    refactor 30.06

--- office_1.py
+++ office_1.py
@@ -1,7 +1,6 @@
 def outed(meet, boss):
     if meet is not None:
-        return 'Get Out Now!' if ((sum(meet.values()) + meet.get(boss) * 2 - meet.get(boss)) / len(
-            meet)) <= 5 else 'Nice Work Champ!'
+        return 'Get Out Now!' if (sum(meet.values()) + meet.get(boss) / len(meet) <= 5 else 'Nice Work Champ!'
 
 
 def outed(meet, boss):
